054b2141db7f55fa72be75cd98ecdb2a2581c859
mykks32 <srikri5462ydv@gmail.com>
2025-04-09 22:30:03 +0545

    feat: footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Introduction from "@/sections/Introduction";
 import Features from "@/sections/Features";
 import Faqs from "@/sections/Faqs";
 import CallToAction from "@/sections/CallToAction";
+import Footer from "@/sections/Footer";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Features />
       <Faqs />
       <CallToAction />
+      <Footer />
     </div>
   );
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,27 @@
+import Image from "next/image";
+import Logo from "@/assets/images/logo.svg"
+
 const footerLinks = [
-    { href: "#", label: "Contact" },
-    { href: "#", label: "Privacy Policy" },
-    { href: "#", label: "Terms & Conditions" },
+  { href: "#", label: "Contact" },
+  { href: "#", label: "Privacy Policy" },
+  { href: "#", label: "Terms & Conditions" },
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+  return (
+    <section className="mt-36">
+      <div className="container mx-auto px-4 md:px-8 lg:px-16">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 items-center">
+          <div>
+            <Image src={Logo} alt="Logo" className="h-9 w-auto md:h-auto" />
+          </div>
+          <div className="flex gap-4 text-neutral-500 justify-center">
+            {footerLinks.map((foo, index) => (
+              <div key={index}>{foo.label}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
