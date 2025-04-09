9f85d0788e72896798372236ed48679bb5358e83
mykks32 <srikri5462ydv@gmail.com>
2025-04-10 00:39:18 +0545

    feat: clip added

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -2,42 +2,70 @@ import FeatureCard from "@/components/feature-card";
 import Tag from "@/components/tag";
 
 const features = [
-  "Asset Library",
-  "Code Preview",
-  "Flow Mode",
-  "Smart Sync",
-  "Auto Layout",
-  "Fast Search",
-  "Smart Guides",
+  "Find Tutors",
+  "Schedule Google Meet",
+  "Get Notified",
+  "Easy Payment",
+  "Browse Tutor Profiles",
+  "Set Learning Goals",
+  "View Reviews & Ratings",
+  "Manage Your Schedule",
 ];
 
 export default function Features() {
   return (
-    <section className="py-10">
+    <section className="py-10 bg-pu">
       <div className="w-full mx-auto px-4 md:px-8 lg:px-16">
         <div className="flex justify-center">
           <Tag>Features</Tag>
         </div>
         <h2 className="text-7xl font-medium text-center mt-6">
-          Where power meets{" "}
-          <span className="text-lime-400 md:block">simplicity</span>
+          Simple yet powerful{" "}
+          <span className="text-lime-400 md:block">features</span>
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-3 gap-8 w-full mt-8">
           <FeatureCard
             className="col-span-1 md:col-span-2 lg:col-span-1"
-            name="Real Time Collabration"
-            text="Work together seamlessly with conflict-free team editing"
-          ></FeatureCard>
+            name="Find Tutors"
+            text="Easily search for the right tutor based on your needs."
+          >
+            <video
+              src="/clip.mp4"
+              autoPlay
+              muted
+              loop
+              playsInline
+              className="rounded-xl shadow-lg w-full"
+            />
+          </FeatureCard>
           <FeatureCard
             className="col-span-1 md:col-span-2 lg:col-span-1"
-            name="Real Time Collabration"
-            text="Work together seamlessly with conflict-free team editing"
-          ></FeatureCard>
+            name="Messaging"
+            text="Communicate directly with tutors through messaging."
+          >
+            <video
+              src="/clip.mp4"
+              autoPlay
+              muted
+              loop
+              playsInline
+              className="rounded-xl shadow-lg w-full"
+            />
+          </FeatureCard>
           <FeatureCard
             className="col-span-1 md:col-start-2 md:col-span-2 lg:col-span-1"
-            name="Real Time Collabration"
-            text="Work together seamlessly with conflict-free team editing"
-          ></FeatureCard>
+            name="Schedule Classes"
+            text="Easily schedule sessions with tutors that fit your availability."
+          >
+            <video
+              src="/clip.mp4"
+              autoPlay
+              muted
+              loop
+              playsInline
+              className="rounded-xl shadow-lg w-full"
+            />
+          </FeatureCard>
         </div>
         <div className="mt-8 flex flex-wrap gap-3 justify-center">
           {features.map((feature, index) => (
